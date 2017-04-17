cfa559ae3ee85f8f429538a2d7d75622dd49c499
Jan Svoboda <jan@mluv.cz>
2017-04-17 15:37:37 +0200

    image conversion fixes

--- lib/escpos/image.rb
+++ lib/escpos/image.rb
@@ -1,7 +1,7 @@
 module Escpos
   class Image
 
-    VERSION = "0.0.2"
+    VERSION = "0.0.3"
 
     def initialize(image_path, opts = {})
       if opts.fetch(:convert_to_monochrome, false)
@@ -31,7 +31,10 @@ module Escpos
             ChunkyPNG::Color.b(@image[x, y]),
             ChunkyPNG::Color.a(@image[x, y])
           px = (r + g + b) / 3
-          raise ArgumentError.new("PNG images with alpha are not supported.") unless a == 255
+          # Alpha is flattened with convert_to_monochrome option
+          unless a == 255
+            raise ArgumentError.new("PNG images with alpha are not supported. Use \"convert_to_monochrome\" to flatten it.")
+          end
           value = px > 128 ? 255 : 0
           value = (value << 8) | value
           temp |= mask if value == 0
@@ -53,6 +56,10 @@ module Escpos
       ].join
     end
 
+    def chunky_png_image
+      @image
+    end
+
     private
 
     # !!!
@@ -71,16 +78,35 @@ module Escpos
 
     def convert_to_monochrome(image_path, opts = {})
       image = MiniMagick::Image.open(image_path)
+
+      # Flatten transparency
       image.flatten
-      image.grayscale 'Rec709Luma'
-      if opts.fetch(:dither, true)
-        image.monochrome '+dither'
-      else
-        image.monochrome
+
+      # Get the first image out of animated gifs
+      image.collapse!
+
+      # Optimise more actions to single call
+      image.combine_options do |c|
+        c.rotate opts.fetch(:rotate) if opts.has_key?(:rotate)
+        c.resize opts.fetch(:resize) if opts.has_key?(:resize)
+        c.grayscale 'Rec709Luma'
+        if opts.fetch(:dither, true)
+          c.monochrome '+dither'
+          # dither the image with FloydSteinberg algoritm for better results
+          c.dither 'FloydSteinberg'
+        else
+          c.monochrome '+dither' # + operator disables dithering
+        end
       end
+
+      # Limit the extent of the image to nice round numbers
       if opts.fetch(:extent, true)
         image.extent "#{(image.width/8.0).round*8}x#{(image.height/8.0).round*8}"
       end
+
+      # Force PNG format so ChunkyPNG works
+      image.format 'png'
+
       image
     end
 
